fdec84c214dfbf56dfd245e511fa9632c74e771c
Ozhmegov Egor <egor-ozhmegov@mail.ru>
2017-01-16 09:12:27 +0300

    for each html

--- src/main/java/dao/BookDaoImpl.java
+++ src/main/java/dao/BookDaoImpl.java
@@ -1,90 +1,55 @@
 package dao;
 
 import model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
-import util.HibernateUtil;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import java.util.List;
 
 @Repository
 public class BookDaoImpl implements BookDao {
+    private SessionFactory sessionFactory;
 
     @Override
     public void addBook(Book book) {
-        EntityManager entityManager = null;
-        try {
-            entityManager= HibernateUtil.INSTANCE.getEntityManager();
-            entityManager.getTransaction().begin();
-            entityManager.persist(book);
-            entityManager.getTransaction().commit();
-            entityManager.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(book);
     }
 
     @Override
     public Book getBookById(int id) {
-        EntityManager entityManager = null;
-        Book book = null;
-        try {
-            entityManager = HibernateUtil.INSTANCE.getEntityManager();
-            book = entityManager.find(Book.class, id);
-            entityManager.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return book;
+        Session session = this.sessionFactory.getCurrentSession();
+        return (Book)session.load(Book.class, id);
     }
 
     @Override
     public void removeBook(int id) {
-        EntityManager entityManager = null;
-        try {
-            entityManager= HibernateUtil.INSTANCE.getEntityManager();
-            entityManager.getTransaction().begin();
-            Book book = entityManager.find(Book.class, id);
-            entityManager.remove(book);
-            entityManager.getTransaction().commit();
-            entityManager.close();
-        }catch (Exception e){
-            e.printStackTrace();
+        Session session = this.sessionFactory.getCurrentSession();
+        Book book = (Book) session.load(Book.class, id);
+        if(book != null){
+            session.delete(book);
         }
     }
 
     @Override
     public void updateBook(Book book) {
-        EntityManager entityManager;
-        try{
-            entityManager = HibernateUtil.INSTANCE.getEntityManager();
-            entityManager.getTransaction().begin();
-            if (!entityManager.contains(book)) {
-                entityManager.merge(book);
-            }
-            entityManager.getTransaction().commit();
-            entityManager.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(book);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<Book> listBooks() {
-        List<Book> books = null;
-        EntityManager entityManager = null;
-        try {
-            entityManager= HibernateUtil.INSTANCE.getEntityManager();
-            entityManager.getTransaction().begin();
-            Query query = entityManager.createQuery("SELECT e FROM Book e");
-            books = query.getResultList();
-            entityManager.getTransaction().commit();
-            entityManager.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return books;
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.createQuery("from Book").list();
+    }
+
+    /**
+     * Sets sessionFactory.
+     *
+     * @param sessionFactory the new value.
+     */
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 }
--- src/main/java/service/BookServiceImpl.java
+++ src/main/java/service/BookServiceImpl.java
@@ -2,7 +2,6 @@ package service;
 
 import dao.BookDaoImpl;
 import model.Book;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
