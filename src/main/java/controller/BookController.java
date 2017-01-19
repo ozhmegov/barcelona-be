f47331f1f3ef4a297672afcc0f0b591ee8488f81
Ozhmegov Egor <egor-ozhmegov@mail.ru>
2017-01-20 01:57:48 +0300

    restController

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -5,17 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import service.BookService;
 
-@Controller
+@RestController
 public class BookController {
     private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "redirect:/view/index.html";
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.GET, headers = "Accept=application/json")
     public String listBooks(Model model){
         model.addAttribute("book", new Book());
         model.addAttribute("listBooks", this.bookService.listBooks());
