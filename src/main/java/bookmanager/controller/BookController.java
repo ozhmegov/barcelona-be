07107e9eff76c061f627cae28721687e22360779
Ozhmegov Egor <egor-ozhmegov@mail.ru>
2017-01-25 01:54:36 +0300

    restController

--- src/main/java/bookmanager/controller/BookController.java
+++ src/main/java/bookmanager/controller/BookController.java
@@ -1,8 +1,5 @@
 package bookmanager.controller;
 
-import bookmanager.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/bookmanager/controller/BookRestController.java
+++ src/main/java/bookmanager/controller/BookRestController.java
@@ -7,16 +7,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import bookmanager.service.BookServiceImpl;
-
 import java.util.List;
 
 @RestController
 public class BookRestController {
     private BookService bookService;
 
-    @RequestMapping(value="/view/books",method = RequestMethod.GET)
+    @RequestMapping(value="/view/books.html",method = RequestMethod.GET, headers = "application/json")
     public List<Book> getAllBooks() {
+        System.out.println("LLLL");
         for(Book book : bookService.listBooks()) System.out.println(book);
         return bookService.listBooks();
     }
